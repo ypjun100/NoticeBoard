87374dd9868127a62b4da505dfa2c418c6df09fb
ypjun100 <ypjun100@gmail.com>
2023-04-26 20:37:25 +0900

    게시판 변경 기능 추가 • 게시판을 쉽게 추가하거나 수정 및 삭제가 용이하도록 Property List 제작 • 프로그램 시작시 초기 데이터를 가져올 때에도 로딩창이 뜨도록 구현 • 게시판 선택 관련 변수 이름들 boardSelect... -> boardSelection... 으로 변경

--- NoticeBoard/Controllers/Main/ViewController.swift
+++ NoticeBoard/Controllers/Main/ViewController.swift
@@ -11,53 +11,80 @@ import SwiftSoup
 
 class ViewController: NSViewController {
     
-    @IBOutlet var boardSelectView: NSView!
+    @IBOutlet var boardSelectionView: NSView!
+    @IBOutlet var boardSelectionMenu: NSMenu!
     @IBOutlet var scrollView: NSScrollView!
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var progressIndicator: NSProgressIndicator!
     
+    var boardUrls: [[String]] = []
+    var currentBoardSelectionIndex = 0
     let visitedNoticeManager = VisitedNoticeManager() // 게시글 방문여부 확인 매니저
-    var pageIndex = 0 // 게시판 페이지 인덱스
+    var boardPageIndex = 0 // 게시판 페이지 인덱스
     var notices: [Notice] = [] // 게시글
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Board Select View UI 수정
-        boardSelectView.wantsLayer = true
-        boardSelectView.layer?.cornerRadius = 5.0
-        boardSelectView.layer?.backgroundColor = CGColor(gray: 1.0, alpha: 0.05)
+        boardSelectionView.wantsLayer = true
+        boardSelectionView.layer?.cornerRadius = 5.0
+        boardSelectionView.layer?.backgroundColor = CGColor(gray: 1.0, alpha: 0.05)
         
-        // 게시판 데이터 가져오기
-        BoardParser.parse(url: "https://home.sch.ac.kr/sch/06/010100.jsp", pageIndex: pageIndex) { notices in
-            self.notices = notices
-            self.tableView.reloadData()
+        // 게시판 리스트 가져오기
+        if let URL = Bundle.main.url(forResource: "BoardUrl", withExtension: "plist") {
+            if let data = NSArray(contentsOf: URL) as? [NSArray] {
+                for (i, elem) in data.enumerated() {
+                    boardUrls.append(elem as! Array<String>)
+                    boardSelectionMenu.addItem(withTitle: boardUrls[i][0], action: nil, keyEquivalent: "")
+                }
+            }
         }
+        
+        updateBoardData()
         
         tableView.action = #selector(onItemClicked) // 테이블 요소 선택시 액션
         
-        NotificationCenter.default.addObserver(self, selector: #selector(onScrollEnded), name: NSScrollView.didEndLiveScrollNotification, object: nil) // 아래로 스크롤 시 알림
+        NotificationCenter.default.addObserver(self, selector: #selector(onScrollEnded), name: NSScrollView.didEndLiveScrollNotification, object: nil) // 아래로 스크롤 시
         
-        visitedNoticeManager.removeAll() // 테스트용
+//        visitedNoticeManager.removeAll() // 전체 삭제
+    }
+    
+    // 게시판 변경시
+    @IBAction func onBoardSelectionChagned(_ sender: NSPopUpButton) {
+        currentBoardSelectionIndex = sender.indexOfSelectedItem
+        boardPageIndex = 0
+        clearBoardData()
+        updateBoardData()
     }
     
     // 게시글 행 클릭시
     @objc func onItemClicked() {
         visitedNoticeManager.addNotice(noticeId: notices[tableView.clickedRow].id)
-        NSWorkspace.shared.open(URL(string: "https://home.sch.ac.kr/sch/06/010100.jsp" + notices[tableView.clickedRow].url)!)
+        NSWorkspace.shared.open(URL(string: boardUrls[currentBoardSelectionIndex][1] + notices[tableView.clickedRow].url)!)
         tableView.reloadData(forRowIndexes: IndexSet(arrayLiteral: tableView.clickedRow), columnIndexes: IndexSet(integer: 0))
     }
     
     // 사용자 스크롤 종료시
     @objc func onScrollEnded() {
         if(scrollView.contentView.bounds.origin.y + scrollView.contentView.bounds.height == scrollView.documentView?.bounds.height) {
-            progressIndicator.startAnimation(self)
-            pageIndex += 1
-            BoardParser.parse(url: "https://home.sch.ac.kr/sch/06/010100.jsp", pageIndex: pageIndex) { notices in
-                self.notices.append(contentsOf: notices)
-                self.tableView.reloadData()
-                self.progressIndicator.stopAnimation(self)
-            }
+            boardPageIndex += 1
+            updateBoardData()
+        }
+    }
+    
+    func clearBoardData() {
+        self.notices = []
+        self.tableView.reloadData()
+    }
+    
+    // 게시판 데이터 가져오기
+    func updateBoardData() {
+        progressIndicator.startAnimation(self)
+        BoardParser.parse(url: boardUrls[currentBoardSelectionIndex][1], pageIndex: boardPageIndex) { notices in
+            self.notices.append(contentsOf: notices)
+            self.tableView.reloadData()
+            self.progressIndicator.stopAnimation(self)
         }
     }
 }
